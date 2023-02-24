e22e48de0633e0cc09fbedc0f7ba942c39f76942
Sean Aubin <sean@proteinqure.com>
2023-02-24 15:59:10 -0500

    remove redundant pointers

--- cumsum.zig
+++ cumsum.zig
@@ -1,16 +1,18 @@
 const std = @import("std");
 
-pub fn cumsum(probs: *[]const f64, cdf: *[]f64) void {
+pub fn cumsum(probs: []const f64, cdf: []f64) void {
+    
+
     // cumulative sum
     var total: f64 = 0;
-    for (probs.*, 0..) |prob, index| {
+    for (probs, 0..) |prob, index| {
         total = total + prob;
-        cdf.*[index] = total;
+        cdf[index] = total;
     }
 
     // normalize by the total because floating point arithmetic, even though the total should be 1.0
-    for (cdf.*, 0..) |_, index| {
-        cdf.*[index] = cdf.*[index] / total;
+    for (cdf, 0..) |_, index| {
+        cdf[index] = cdf[index] / total;
     }
 }
 
@@ -23,14 +25,14 @@ pub fn main() !void {
     var probs: []const f64 = &[_]f64{ 0.2, 0.2, 0.1, 0.1, 0.4 };
     var cdf = try allocator.alloc(f64, probs.len);
 
-    cumsum(&probs, &cdf);
+    cumsum(probs, cdf);
     std.debug.print("probs:\n", .{});
     for (probs) |prob| {
-        std.debug.print("{} ", .{prob});
+        std.debug.print("{d:.1} ", .{prob});
     }
 
     std.debug.print("\ncdfs:\n", .{});
     for (cdf) |cum| {
-        std.debug.print("{} ", .{cum});
+        std.debug.print("{d:.1} ", .{cum});
     }
 }
