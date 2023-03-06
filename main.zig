f6c20610014cb2fea14a459301fc050bc8d8ede8
Sean Aubin <sean@proteinqure.com>
2023-03-06 16:22:45 -0500

    min.if noq euna

--- main.zig
+++ main.zig
@@ -5,11 +5,11 @@ const std = @import("std");
 // - a list of dictionaries where each letter has it's own probability
 // - a matrix of probabilities + a list of row labels
 
-// TODO: how do I set array the type to be any numerical type?
 // copied from Python's bisect_right
-pub fn searchSorted(arr: *[]const f64, sort_val: f64) usize {
+pub fn searchSorted(arr: []const f64, sort_val: f64) usize {
     var low: usize = 0;
-    var high = arr.length;
+    var high = arr.len;
+
     while (low < high) {
         const mid = @divFloor(low + high, 2);
 
@@ -18,54 +18,59 @@ pub fn searchSorted(arr: *[]const f64, sort_val: f64) usize {
         }
         else
         {
-            low = mid;
+            low = mid + 1;
         }
     }
     return low;
 }
 
 
-// lol, I have no idea what type to use for the probabilities to be compatible with Python
-pub fn chooseWeighted(comptime T: type, arr: *[]const T, rand: *const std.rand.Random, p: []const f64) T {
-    var cdf = std.mem.zeroes([std.mem.len(arr)]f64);
-    //var cdf = [_]f64{0} * arr.*.len;
+pub fn cumsum(probs: []const f64, cdf: []f64) void {
+    std.debug.assert(probs.len == cdf.len);
 
-
-    // cumsum
+    // cumulative sum
     var total: f64 = 0;
-    for (p) |prob, index| {
+    for (probs, 0..) |prob, index| {
         total = total + prob;
         cdf[index] = total;
     }
 
     // normalize by the total because floating point arithmetic, even though the total should be 1.0
-    for (cdf) |_, index| {
+    for (cdf, 0..) |_, index| {
         cdf[index] = cdf[index] / total;
     }
+}
 
-    // how do I make an empty array of a given size?
-    // should this be a constant?
-    var chosen = [arr.len]T{};
 
-    // this is where the parallel could happen! I think I want a threadpool and not os threads though?
-    for (chosen) |_, index| {
-        // generate several random floats rand.float
-        // get index using searchsorted
-        chosen[index] = arr.*[searchSorted(arr, rand.float())];
+pub fn chooseWeighted(comptime T: type, arr: []const T, probs: []const f64, cdf: []f64, chosen: []T, rand: *const std.rand.Random) void {
+    cumsum(probs, cdf);
+
+    for (chosen, 0..) |_, index| {
+        chosen[index] = arr[searchSorted(cdf, rand.float(f64))];
     }
-    
-    return chosen;
 }
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     var prng = std.rand.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.os.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
     });
+
     const rand = prng.random();
+
     var arr: []const u8 = "world";
-    const prob: []const f64 = &[_]f64{0.2, 0.2, 0.1, 0.1, 0.4};
-    const w_item = chooseWeighted(u8, &arr, &rand, prob);
-    std.debug.print("Hello, {c}\n", .{w_item});
+    const probs: []const f64 = &[_]f64{0.2, 0.2, 0.1, 0.1, 0.4};
+    const cdf = try allocator.alloc(f64, probs.len);
+    const chosen = try allocator.alloc(u8, 10000);
+
+    chooseWeighted(u8, arr, probs, cdf, chosen, &rand);
+    for (chosen) |char| {
+        std.debug.print("{c} ", .{char});
+    }
 }
